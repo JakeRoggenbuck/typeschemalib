8bbdc42b3b43757a613fff875885f5df8fe0275a
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2020-09-23 00:15:29 -0700

    Add comments in readme and test

--- test.py
+++ test.py
@@ -4,14 +4,17 @@ from typeschemalib import typeschemalib
 if __name__ == "__main__":
     data = {"point": 45, "my_string": "Hey", "grade": 4.5}
 
+    # Validate data from schema file
     schema = "test.stml"
     valid = typeschemalib.schema_check(schema, data)
     print(valid)
 
-    schema = ["point: Int", "my_string: Str", "grade: Float"]
+    # Validate data from list of schema
+    schema = ["point: Int", "my_string: Str", "grade: Int"]
     valid = typeschemalib.schema_check(schema, data)
     print(valid)
 
-    schema = {"point": "Int", "my_string": "Str", "grade": "Float"}
+    # Validate data from dict of values
+    schema = {"point": "Int", "my_string": "Str", "grade": "Int"}
     valid = typeschemalib.schema_check(schema, data)
     print(valid)
